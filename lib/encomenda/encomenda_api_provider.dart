e798be358f89aef216afdcbdcb100b77985a4f12
guirruta <yotlanga@gmail.com>
2020-02-11 16:27:44 +0200

    #11 esqueleto do metodo para envio de encomenda pela rede

--- lib/encomenda/encomenda_api_provider.dart
+++ lib/encomenda/encomenda_api_provider.dart
@@ -1,22 +1,22 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:primobile/Database/Database.dart';
-
 import 'encomenda_modelo.dart';
-
+import 'package:http/http.dart' as http;
 
 class EncomendapiProvider {
-
-Future<List<Encomenda>> getTodasEncomendas(String  token) async {
+  Future<List<Encomenda>> getTodasEncomendas(String token) async {
     var url = 'https://5ac64b0b.ngrok.io/api/encomendas';
     Response response;
 
     try {
-     response = await Dio().get( url,  options: Options(
-       headers: {
-         "x-access-token": 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwZXJmaWwiOiJhZG1pbiIsIm5vbWUiOiJqbXJhZG1pbiIsImlhdCI6MTU3ODQ5NDY5NCwiZXhwIjoxNTc4NDk0OTk0fQ.SwrH7RQT3TbbIUzaaQe6ZSVkiSlagB_WItc3fqqwm1E'
-       }
-     ) );
-      
+      response = await Dio().get(url,
+          options: Options(headers: {
+            "x-access-token":
+                'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwZXJmaWwiOiJhZG1pbiIsIm5vbWUiOiJqbXJhZG1pbiIsImlhdCI6MTU3ODQ5NDY5NCwiZXhwIjoxNTc4NDk0OTk0fQ.SwrH7RQT3TbbIUzaaQe6ZSVkiSlagB_WItc3fqqwm1E'
+          }));
+
       print('response');
       print(response);
     } on DioError catch (e) {
@@ -31,9 +31,19 @@ Future<List<Encomenda>> getTodasEncomendas(String  token) async {
     }).toList();
   }
 
-
-
   void insertEncomenda(Encomenda encomenda) async {
-      await DBProvider.db.insertEncomenda(encomenda);    
+    await DBProvider.db.insertEncomenda(encomenda);
+  }
+
+  Future<http.Response> postEncomenda(Encomenda encomenda) async {
+    var url = 'https://api/encomenda/';
+    var body = json.encode(encomenda.toMap());
+
+    var response = await http.post(url,
+        headers: {"Content-Type": "application/json"}, body: body);
+
+    print("${response.statusCode}");
+    print("${response.body}");
+    return response;
   }
 }
